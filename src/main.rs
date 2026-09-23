28518aebdcdafba1e5032b612d2fcc5d09957cf4
vkini19 <varnkini@gmail.com>
2026-09-23 18:13:43 -0400

    Wrote testing functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+fn test_product(id: String){
+    println!("Product ID: {}", id);
+}
+
+fn test_quantity(quantity: i32){
+    print!("Quantity: {}", quantity);
+}
+
+fn main(){
+    let product_id = String::from("P001");
+    let quantity: i32 = 10;
+
+    test_product(product_id);
+    test_quantity(quantity);
 }
